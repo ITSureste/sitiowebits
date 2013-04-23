814ad88be63146c3107a6847491cfd9ecf91903a
Edgar Estrella <name_edgar@hotmail.com>
2013-04-22 22:21:31 -0500

    Estructura html

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE hmtl>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<img src="logo.png" alt="ITSureste">
+				<ul>
+					<li><a href="">Inicio</a></li>
+					<li><a href="">Quienes Somos</a></li>
+					<li><a href="">Servicios</a></li>
+					<li><a href="">Contactanos</a></li>
+				</ul>
+			</nav>
+		</header>
+		<h1></h1>
+		<h2></h2>
+		<footer>
+			<nav>
+				<ul>
+					<li><a href="">Mapa del Sitio</a></li>
+				</ul>
+			</nav>
+		</footer>
+	</body>
+</html>
