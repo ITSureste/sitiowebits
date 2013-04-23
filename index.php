872472e759e28d962081ba8ef0d7b609433b8428
pedro salvador <pmsarey@gmail.com>
2013-04-23 01:54:57 -0500

    index actualizado (pmsa)

--- index.php
+++ index.php
@@ -2,28 +2,56 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title></title>
+		<title>IT SURESTE</title>
+		<link rel="stylesheet" href="css/estilo.css" />
 	</head>
 	<body>
-		<header>
-			<nav>
-				<img src="logo.png" alt="ITSureste">
-				<ul>
-					<li><a href="">Inicio</a></li>
-					<li><a href="">Quienes Somos</a></li>
-					<li><a href="">Servicios</a></li>
-					<li><a href="">Contactanos</a></li>
-				</ul>
-			</nav>
-		</header>
-		<h1></h1>
-		<h2></h2>
-		<footer>
-			<nav>
-				<ul>
-					<li><a href="">Mapa del Sitio</a></li>
-				</ul>
-			</nav>
-		</footer>
+		<div id="container">
+			<header>
+				<figure>
+					<a href="index.php">
+						<img src="img/logo.png" />
+					</a>
+					<!--<figcaption>Aqui va el slogan</figcaption>-->
+				</figure>				
+				<nav>				
+					<ul>
+						<li><a href="#">Inicio</a></li>
+						<li><a href="#">Quienes Somos</a></li>
+						<li><a href="#">Servicios</a></li>
+						<li><a href="#">Contactanos</a></li>						
+					</ul>
+				</nav>
+				<div class="clean"></div>
+			</header>
+			
+			<section>
+				<span>Hello section</span>
+				<article id="art1"> Hello Article 1</article>
+				<article id="art2"> Hello Article 2</article>
+			</section>
+				
+			<aside>
+				Hello Aside
+				<p>
+					PMSA: Dejenme el header ya mero lo tengo XD
+					<br/> 
+					<br/> 
+					Al parecer hay un detalle con el "nav" en el IE9, chrome y firefox running.
+					<br/>
+					<br/>
+					Hora entrada: 10:15pm<br/>
+					Hora salida: 2:00am													
+				</p>
+			</aside>
+			
+			<footer>
+				<nav>
+					<ul>
+						<li><a href="#">Mapa del Sitio</a></li>
+					</ul>
+				</nav>
+			</footer>
+		</div>
 	</body>
 </html>
