fbc86ca4a0246acd4130b1b4ffec4c03bdea4dc2
Leonardo Gomez <cientifico_13@hotmail.com>
2013-04-25 21:46:02 -0500

    modifi

--- quien.php
+++ quien.php
@@ -12,7 +12,7 @@
 					<a href="index.php">
 						<img src="img/logo.png" />
 					</a>
-					<!--<figcaption>Aqui va el slogan</figcaption>-->
+					<!--<figcaption>La Innovacion hace la diferencia</figcaption>-->
 				</figure>				
 				<nav>				
 					<ul>
@@ -25,22 +25,53 @@
 				<div class="clean"></div>
 			</header>
 
-			<section>
-				<span>¿Quienes Somos?</span>
-			</section>
+				<section>
+					<span>Mision y Vision</span>
+
+						<img src="img/mi.png" width="600" height="400" alt="logo" />
+
+				</section>
 
 			<aside>
-			<span>Mision</span>
-				<text>informacion</text>
+				<b><span>Empresa</span></b>
+					<text>ITSURESTE nace de la idea de los itsureños</text>
 
 			</aside>
 
-
 			<footer>
 				<nav>
-					<ul>
-						<li><a href="#">Mapa del Sitio</a></li>
-					</ul>
+					
+					<div class="navFooter">
+						<span>Contactos</span>
+						<ul>
+							<li><a href="#">Mapa del Sitio</a></li>
+							<li><a href="#">Que onda</a></li>
+							<li><a href="#">Ciudades</a></li>
+							<li><a href="#">Paises</a></li>
+						</ul>
+					</div>
+					
+					<div class="navFooter">
+						<span>Servicios</span>
+						<ul>
+							<li><a href="#">Mapa del Sitio</a></li>
+							<li><a href="#">Que onda</a></li>
+							<li><a href="#">Ciudades</a></li>
+							<li><a href="#">Paises</a></li>
+						</ul>
+					</div>
+					
+					<div class="navFooter">
+						<span>Oficinas</span>
+						<ul>
+							<li><a href="#">Mapa del Sitio</a></li>
+							<li><a href="#">Que onda</a></li>
+							<li><a href="#">Ciudades</a></li>
+							<li><a href="#">Paises</a></li>
+						</ul>
+					</div>
+					
+					<div class="clean"></div>
 				</nav>
 			</footer>
 		</div>
