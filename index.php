fb63f6c1ceeee2c040e13e4c24612be885ea035d
Leonardo Gomez <cientifico_13@hotmail.com>
2013-04-30 18:22:00 -0500

    ITSUREÑOS

--- index.php
+++ index.php
@@ -16,12 +16,8 @@
 				<img src="img/ini.png" width="1000px"/>
 			</section>
 			
-			<section>
-				<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FInnovaciones-Tecnol%25C3%25B3gicas-del-Sureste%2F471016546305070%3Fref%3Dstream&amp;width=350&amp;height=290&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color&amp;header=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:290px;" allowTransparency="true"></iframe>
-			</section>
-			
-			<aside>
-				<a class="twitter-timeline" href="https://twitter.com/ITSureste" data-widget-id="329369625756311552">Tweets por @ITSureste</a>
+			<section><iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FInnovaciones-Tecnol%25C3%25B3gicas-del-Sureste%2F471016546305070%3Fref%3Dstream&amp;width=250&amp;height=290&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color&amp;header=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:250px; height:290px;" allowTransparency="true"></iframe></section>
+				<aside><a class="twitter-timeline" href="https://twitter.com/ITSureste" data-widget-id="329369625756311552">Tweets por @ITSureste</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>	
 			</aside>
 			
