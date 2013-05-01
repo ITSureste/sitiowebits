6b17983f23a439fbb6462d73186e3dbcdf662e81
pedro salvador <pmsarey@gmail.com>
2013-04-30 23:59:00 -0500

    pedro

--- footer.php
+++ footer.php
@@ -1,37 +1,37 @@
-		<footer>
-			<nav>
-				
-				<div class="navFooter">
-					<span>Contactos</span>
-					<ul>
-						<li><a href="#">Mapa del Sitio</a></li>
-						<li><a href="#">Que onda</a></li>
-						<li><a href="#">Ciudades</a></li>
-						<li><a href="#">Paises</a></li>
-					</ul>
-				</div>
-				
-				<div class="navFooter">
-					<span>Servicios</span>
-					<ul>
-						<li><a href="#">Mapa del Sitio</a></li>
-						<li><a href="#">Que onda</a></li>
-						<li><a href="#">Ciudades</a></li>
-						<li><a href="#">Paises</a></li>
-					</ul>
-				</div>
-				
-				<div class="navFooter">
-					<span>Oficinas</span>
-					<ul>
-						<li><a href="#">Mapa del Sitio</a></li>
-						<li><a href="#">Que onda</a></li>
-						<li><a href="#">Ciudades</a></li>
-						<li><a href="#">Paises</a></li>
-					</ul>
-				</div>
-				
-				<div class="clean"></div>
-			</nav>
-		</footer>
+<footer>
+	<nav>
+		
+		<div class="navFooter">
+			<span>Contactos</span>
+			<ul>
+				<li><a href="#">Mapa del Sitio</a></li>
+				<li><a href="#">Que onda</a></li>
+				<li><a href="#">Ciudades</a></li>
+				<li><a href="#">Paises</a></li>
+			</ul>
+		</div>
+		
+		<div class="navFooter">
+			<span>Servicios</span>
+			<ul>
+				<li><a href="#">Mapa del Sitio</a></li>
+				<li><a href="#">Que onda</a></li>
+				<li><a href="#">Ciudades</a></li>
+				<li><a href="#">Paises</a></li>
+			</ul>
+		</div>
+		
+		<div class="navFooter">
+			<span>Oficinas</span>
+			<ul>
+				<li><a href="#">Mapa del Sitio</a></li>
+				<li><a href="#">Que onda</a></li>
+				<li><a href="#">Ciudades</a></li>
+				<li><a href="#">Paises</a></li>
+			</ul>
+		</div>
+		
+		<div class="clean"></div>
+	</nav>
+</footer>
 		
--- header.php
+++ header.php
@@ -1,41 +1,41 @@
-		<header>
-			<figure>
-				<a href="index.php">
-					<img src="img/logo.png" />
-				</a>
-				<!--<figcaption>Aqui va el slogan</figcaption>-->
-			</figure>				
-			<nav id="navigation">				
-				<ul>
-					<li>
-						<a id="idIndex" href="index.php">Inicio</a>
-					</li>
-					<li>
-						<a id="idServicio" class="navShow">Servicios</a>
-						<div class="navChildren">
-							<div class="navChildrenNext">
-								Servicios
-							</div>
-						</div>
-					</li>
-					<li>
-						<a id="idNosotros" class="navShow">Nosotros</a>
-						<div class="navChildren">
-							<div class="navChildrenNext">
-								Nosotros
-							</div>
-						</div>
-					</li>
-					<li>
-						<a id="idContacto" class="navShow">Contactanos</a>
-						<div class="navChildren">								
-							<div class="navChildrenNext">
-								Contactanos
-							</div>
-						</div>
-					</li>
-				</ul>					
-			</nav>
-			<div class="clean"></div>
-		</header>
+<header>
+	<figure>
+		<a href="index.php">
+			<img src="img/logo.png" />
+		</a>
+		<!--<figcaption>Aqui va el slogan</figcaption>-->
+	</figure>				
+	<nav id="navigation">				
+		<ul>
+			<li>
+				<a id="idIndex" href="index.php">Inicio</a>
+			</li>
+			<li>
+				<a id="idServicio" class="navShow">Servicios</a>
+				<div class="navChildren">
+					<div class="navChildrenNext">
+						Servicios
+					</div>
+				</div>
+			</li>
+			<li>
+				<a id="idNosotros" class="navShow">Nosotros</a>
+				<div class="navChildren">
+					<div class="navChildrenNext">
+						Nosotros
+					</div>
+				</div>
+			</li>
+			<li>
+				<a id="idContacto" class="navShow">Contactanos</a>
+				<div class="navChildren">								
+					<div class="navChildrenNext">
+						Contactanos
+					</div>
+				</div>
+			</li>
+		</ul>					
+	</nav>
+	<div class="clean"></div>
+</header>
 		
