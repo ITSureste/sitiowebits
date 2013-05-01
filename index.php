98de4670df586bc87ef298f51183bf6cfc44068d
pedro salvador <pmsarey@gmail.com>
2013-04-30 23:58:33 -0500

    pedro

--- index.php
+++ index.php
@@ -8,25 +8,61 @@
 
 	</head>
 	<body>
-		<div id="container">
+		<div id="contenedor">
 			
 <?php include "header.php"?>
-			
-			<section>
-				<img src="img/ini.png" width="1000px"/>
-			</section>
-			
-			<section><iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FInnovaciones-Tecnol%25C3%25B3gicas-del-Sureste%2F471016546305070%3Fref%3Dstream&amp;width=250&amp;height=290&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color&amp;header=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:250px; height:290px;" allowTransparency="true"></iframe></section>
-				<aside><a class="twitter-timeline" href="https://twitter.com/ITSureste" data-widget-id="329369625756311552">Tweets por @ITSureste</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>	
-			</aside>
-			
-			<section>
-				<img src="img/ini2.png" width="1000px"/>
-			</section>
-
-<?php include "footer.php" ?>
-
+			<div class="efecto"></div>			
+			<div id="contenedorDatos">
+				<section style="width:100%;">
+					<img src="img/ini.png" width="1000px"/>
+				</section>						
+				<section>
+					<span>Hello section</span>
+					<article id="art1">
+						<p style="padding:10px;">					
+							Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+							Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,
+							cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una 
+							galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+							No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, 
+							quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", 
+							las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición,
+							como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.												
+						</p>
+					</article>
+					<article id="art2">
+						<p style="padding:10px;">					
+							Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+							Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,
+							cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una 
+							galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+							No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, 
+							quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", 
+							las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición,
+							como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.												
+						</p>
+					</article>
+				</section>
+					
+					
+				<aside>
+					<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FInnovaciones-Tecnol%25C3%25B3gicas-del-Sureste%2F471016546305070%3Fref%3Dstream&amp;width=250&amp;height=290&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color&amp;header=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:250px; height:290px;" allowTransparency="true">
+					</iframe>
+				</aside>
+				
+				<aside>
+					<a class="twitter-timeline" href="https://twitter.com/ITSureste" data-widget-id="329369625756311552">Tweets por @ITSureste</a>
+					<script>
+						!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+							if(!d.getElementById(id)){
+								js=d.createElement(s);
+								js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+								fjs.parentNode.insertBefore(js,fjs);
+							}}(document,"script","twitter-wjs");
+					</script>
+				</aside>		
+				<?php include "footer.php" ?>
+			</div>	
 		</div>
 	</body>
 </html>
