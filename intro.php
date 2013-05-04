68f94549cb45c591feff6f75ef7e23ac48fe14de
Leonardo Gomez <cientifico_13@hotmail.com>
2013-05-04 10:11:28 -0500

    verificar ordenamiento

---
+++ intro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bienvenido a ITSureste</title>
+		<link rel="stylesheet" href="css/estilo2.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+	</head>
+	<body>
+		<div class="content"><h1>aqui debe cargar el index..las imagenes que estan al inicio son ilustrativas hare un diseño de ITSureste.</h1></div>
+		<div class="leftdoor"></div>
+		<div class="rightdoor"></div>
+		<a class="button"> 
+			<span>Entra</span>
+		</a>
+		<script>
+			$('.button').mousedown(function(){
+			    $(this).addClass("rotate");
+			    setTimeout(function(){
+			        $('.button').addClass("del");
+			    },1900);
+			    $('.leftdoor').addClass("moveleft");
+			    $('.rightdoor').addClass("moveright");
+			    setTimeout(function(){
+			        $('.rightdoor').addClass("del");
+			    },4000);
+			});
+		</script>
+	</body>
+</html>
+
