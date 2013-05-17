1c9e1582b8f785ab60671205ed99fc72c0502986
pedro salvador <pmsarey@gmail.com>
2013-05-16 22:42:34 -0500

    a

--- archivos-globales/pie.php
+++ archivos-globales/pie.php
@@ -2,10 +2,26 @@
 	<div class="navFooter">
 		<span>SIGUENOS</span>		
 		<ul>
-			<li><a href="#">Mapa del Sitio</a></li>
-			<li><a href="#">Que onda</a></li>
-			<li><a href="#">Ciudades</a></li>
-			<li><a href="#">Paises</a></li>
+			<li>
+				<img src="img/icon-facebook.png" />
+				<a href="#">Facebook</a>
+			</li>
+			<li>
+				<img src="img/icon-twitter.png" />
+				<a href="#">Twitter</a>
+			</li>
+			<li>
+				<img src="img/icon-youtube.png" />
+				<a href="#">Youtube</a>
+			</li>
+			<li>
+				<img src="img/icon-linkedin.png" />
+				<a href="#">LinkedIn</a>
+			</li>
+			<li>
+				<img src="img/icon-skype.png" />
+				<a href="#">Skype</a>
+			</li>			
 		</ul>		
 	</div>
 	
